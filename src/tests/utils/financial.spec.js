4d45ff1039b49640a13adfd20a358c6875477bda
Miguel Angel Carrasco <tlacaelel.software@gmail.com>
2024-10-16 18:36:11 -0600

    Adding savings financial.js function

--- src/tests/utils/financial.spec.js
+++ src/tests/utils/financial.spec.js
@@ -1,6 +1,10 @@
 import {expect, test} from 'vitest'
-import {getAnnuityFutureValue} from '../../utils/financial.js'
+import {getAnnuityFutureValue, getSavingsFutureValue} from '../../utils/financial.js'
 
 test('It must calculate the future value of an annuity', () => {
     expect(getAnnuityFutureValue(1000, 0.1, 10)).toBe(15937.424601000022)
+})
+
+test('it must calculate the future value of savings periodic deposits', () => {
+    expect(getSavingsFutureValue(1000, 1000, 0.1, 12, 10)).toBe(207552.02039433146)
 })
--- src/utils/financial.js
+++ src/utils/financial.js
@@ -1,3 +1,19 @@
-export function getAnnuityFutureValue(annuity, interestRate, periods){
+/**
+ * Get the future value of a series of payments made at equal intervals
+ * @param annuity the annuity payment per period
+ * @param interestRate interest rate per period
+ * @param periods the number of periods
+ * @returns {number} the future value of the annuity
+ */
+export function getAnnuityFutureValue(annuity, interestRate, periods) {
     return (annuity * (Math.pow((1 + interestRate), periods) - 1)) / interestRate;
+}
+
+export function getSavingsFutureValue(initialBalance, periodicDeposit, annualInterestRate, paymentsPerYear, years) {
+    let adjustedNumberOfPeriods = years * paymentsPerYear;
+    let adjustedInterestRate = annualInterestRate / paymentsPerYear;
+    let initialBalanceFutureValue = initialBalance * Math.pow(1 + adjustedInterestRate, adjustedNumberOfPeriods);
+    let periodicPaymentsFutureValue = getAnnuityFutureValue(periodicDeposit,
+        adjustedInterestRate, adjustedNumberOfPeriods);
+    return initialBalanceFutureValue + periodicPaymentsFutureValue;
 }
