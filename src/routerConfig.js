0c0eda937056226b6d9fcda7267d053f98e1e3ae
Miguel Angel Carrasco <tlacaelel.software@gmail.com>
2026-05-29 16:50:09 -0600

    Fixing routerConfig.js to support hash based routes for gh-pages compability

--- src/routerConfig.js
+++ src/routerConfig.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AnnuityCalculator from './components/AnnuityCalculator.vue'
 import SavingsCalculator from './components/SavingsCalculator.vue'
 
@@ -20,6 +20,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
